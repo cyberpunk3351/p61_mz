9b1081ae7a24fd72a63d0d94a44c6fa85f6ce960
Drummer <drummer.ilya@gmail.com>
2025-11-25 10:17:34 +0400

    refactor: update home route to redirect to dashboard and adjust related tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -11,8 +11,8 @@ class ExampleTest extends TestCase
 
     public function test_returns_a_successful_response()
     {
-        $response = $this->get(route('home'));
+        $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertRedirect('/dashboard');
     }
 }
