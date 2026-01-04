f38ba10d07c2ad49211c024a060702a3c150b357
Drummer <drummer.ilya@gmail.com>
2026-01-04 13:12:34 +0400

    refactor: use fluent route group syntax

--- routes/playlist.php
+++ routes/playlist.php
@@ -6,11 +6,7 @@ use App\Http\Controllers\Playlist\SearchController;
 use App\Http\Controllers\Playlist\ShowController;
 use Illuminate\Support\Facades\Route;
 
-Route::group([
-    'middleware' => ['auth', 'verified'],
-    'prefix' => 'playlists',
-    'as' => 'playlists.',
-], function () {
+Route::middleware(['auth', 'verified'])->prefix('playlists')->name('playlists.')->group(function () {
     Route::get('/', GetController::class)->name('get');
     Route::get('/{playlist}', ShowController::class)->name('show');
     Route::delete('/{playlist}/tracks/{track}', DetachTrackController::class)->name('tracks.detach');
